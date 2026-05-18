547e9a64dae72250bec475723d4457f0d22c84f1
MThushar <thusharacharya6@gmail.com>
2026-05-18 22:28:12 +0530

    pascals triangle 2

---
+++ may18.py
@@ -0,0 +1,20 @@
+def getRow(rowIndex):
+    row = [1]
+
+    for i in range(1, rowIndex + 1):
+        new_row = [1]
+
+        for j in range(1, i):
+            new_row.append(row[j - 1] + row[j])
+
+        new_row.append(1)
+        row = new_row
+
+    return row
+
+rowIndex = int(input("Enter row index: "))
+
+result = getRow(rowIndex)
+
+print("Pascal's Triangle Row:")
+print(result)
