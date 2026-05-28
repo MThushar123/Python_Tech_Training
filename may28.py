c7fd0757d4c02fd69577ed595cc857fb2e7513cc
MThushar <thusharacharya6@gmail.com>
2026-05-28 10:45:22 +0530

    palindrome partitioning

---
+++ may28.py
@@ -0,0 +1,30 @@
+
+def partition(s):
+    result = []
+
+    def isPalindrome(sub):
+        return sub == sub[::-1]
+
+    def backtrack(start, path):
+        if start == len(s):
+            result.append(path[:])
+            return
+
+        for end in range(start + 1, len(s) + 1):
+            substring = s[start:end]
+
+            if isPalindrome(substring):
+                path.append(substring)
+                backtrack(end, path)
+                path.pop()
+
+    backtrack(0, [])
+    return result
+
+s = input("Enter a string: ")
+
+result = partition(s)
+
+print("Palindrome Partitions:")
+for item in result:
+    print(item)
