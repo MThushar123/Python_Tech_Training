a6d1ab1b8f576f4c93543ad8cf2e61d7380514ad
MThushar <thusharacharya6@gmail.com>
2026-05-29 12:20:07 +0530

    finding the peak element

---
+++ may29.py
@@ -0,0 +1,20 @@
+def findPeakElement(nums):
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if nums[mid] > nums[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return left  
+
+nums = list(map(int, input("Enter array elements: ").split()))
+
+result = findPeakElement(nums)
+
+print("Peak Element Index:", result)
+print("Peak Element:", nums[result])
