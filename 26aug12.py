11e0f6e9309c331398b0e4faed84f4cc406f9b40
MThushar <thusharacharya6@gmail.com>
2026-08-12 16:47:32 +0530

    left half pyramid

---
+++ 26aug12.py
@@ -0,0 +1,7 @@
+N = int(input("Enter the number of pattern to be printed-->"))
+for i in range(1, N + 1):
+	for j in range(1, N - i + 1):
+		print(" ", end="")
+	for j in range(1, i + 1):
+		print("*", end="")
+	print()
