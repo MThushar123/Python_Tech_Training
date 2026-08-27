7a82a2e4af6b49ba0e82b1d79f2020bffc6d08b7
MThushar <thusharacharya6@gmail.com>
2026-08-27 19:19:28 +0530

    arrow pattern

---
+++ 26aug27.py
@@ -0,0 +1,22 @@
+def print_arrow(n):
+    for i in range(1, n):
+        for j in range(0, i):
+            print(" ", end="")
+
+        for k in range(0, i):
+            print("*", end="")
+
+        print()
+
+    for i in range(0, n):
+        for j in range(0, n - i):
+            print(" ", end="")
+
+        for k in range(0, n - i):
+            print("*", end="")
+
+        print()
+
+n = int(input("Enter the value of n: "))
+
+print_arrow(n)
